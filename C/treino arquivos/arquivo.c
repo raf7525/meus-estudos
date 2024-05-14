e3fabc7a9c505917e88c803b9e47c51c874aba1b
Rafa <ralb@cesar.school>
2024-05-14 10:08:55 -0300

    criação do código de escrever em arquivos

--- C/treino arquivos/arquivo.c
+++ C/treino arquivos/arquivo.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
-//Codigo adaptado de (com mais explicacoes sobre fseek) - https://www.geeksforgeeks.org/fseek-in-c-with-example/ e https://www.educba.com/fseek-in-c/ 
 
 int main(void) {
   FILE *fp;
-  fp = fopen("arquivoo.txt","r");
+  fp = fopen("arquivoo.txt", "w");
   char ch;
-  //movendo o ponteiro para o final do arquivo
-  fseek(fp,0,SEEK_SET);
-  //printando o numero de bytes
-  printf("%ld\n", ftell(fp));
-  //movendo agora o ponteiro para una distancia de 20 bytes do inicio do arquivo
-  fseek(fp,-50,SEEK_END);
-  while((ch=fgetc(fp)) != EOF){ //lendo tudo a partir do 20 caracter e imprimindo na tela 
-    putchar(ch);
+  
+  if (fp != NULL) {  // Check if file is opened successfully
+    fprintf(fp, "Hello, this is a test!\n");
+    fputs("Hello, this is a test!\n", fp);
+
+    fseek(fp, -50, SEEK_END);  // Move pointer 50 bytes from the end
+    while ((ch = fgetc(fp)) != EOF) { // Read and print from the current position to the end
+      putchar(ch);
+    }
+
+    fclose(fp);  // Close the file after use
+  } else {
+    printf("Failed to open the file.\n");
   }
 
- /* while(ftell(fp) <= 110){ //lendo tudo a partir do 20 caracter e imprimindo na tela 
-    ch = fgetc(fp);
-    putchar(ch);
-  }*/
-  
   return 0;
 }
